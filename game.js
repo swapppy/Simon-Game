f3da83dafaeaf1345e15ca08f37f542c8bdfaa75
swapppy <swapnilnyayade123@gmail.com>
2021-06-30 18:48:07 +0530

    added click for title

--- game.js
+++ game.js
@@ -6,6 +6,14 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+$("#level-title").click(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
